01b38b9ec9ee563a5f6b53e7f04ec86a1c784b15
Hunter Harris <harrihun000@ksu.edu>
2022-02-22 14:10:54 -0600

    book form backend update

--- E1-CD_Implementation/E1-CD_Implementation/uxBook.cs
+++ E1-CD_Implementation/E1-CD_Implementation/uxBook.cs
@@ -19,21 +19,25 @@ namespace E1_CD_Implementation
             InitializeComponent();
 
             book = b;
+            uxPage.Text = book.Title + "\n\n\n\n" + book.CurrentPage + " / " + book.PageCount;
         }
 
         private void uxPageForward_Click(object sender, EventArgs e)
         {
             book.NextPage();
+            uxPage.Text = book.Title + "\n\n\n\n" + book.CurrentPage + " / " + book.PageCount;
         }
 
         private void uxPageBackward_Click(object sender, EventArgs e)
         {
             book.PrevPage();
+            uxPage.Text = book.Title + "\n\n\n\n" + book.CurrentPage + " / " + book.PageCount;
         }
 
         private void uxAddBookMark_Click(object sender, EventArgs e)
         {
             book.SetBookMark(book.GetBookMarks().Length, book.CurrentPage);
+            uxPage.Text = book.Title + "\n\n\n\n" + book.CurrentPage + " / " + book.PageCount;
         }
     }
 }
