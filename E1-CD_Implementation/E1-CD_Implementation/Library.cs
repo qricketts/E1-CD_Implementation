73f76eb9fe86468c45b172a6ba935a5cebfd03ff
smhurt158 <smhurt@ksu.edu>
2022-02-22 11:08:37 -0600

    started model class

---
+++ E1-CD_Implementation/E1-CD_Implementation/Library.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1_CD_Implementation
+{
+    public class Library
+    {
+
+    }
+}
