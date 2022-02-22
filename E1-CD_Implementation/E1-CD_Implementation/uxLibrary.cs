ae7b6eea482b8c14851aad498fd9c41ac8c72e82
smhurt158 <smhurt@ksu.edu>
2022-02-22 12:31:24 -0600

    commit

--- E1-CD_Implementation/E1-CD_Implementation/uxLibrary.cs
+++ E1-CD_Implementation/E1-CD_Implementation/uxLibrary.cs
@@ -12,10 +12,30 @@ namespace E1_CD_Implementation
 {
     public partial class uxLibrary : Form
     {
+        public BindingList<Book> books = new BindingList<Book>();
+
         public uxLibrary()
         {
             InitializeComponent();
             //this is a test- sean hurt
+
+            uxBookList.DataSource = books;
+
+            if (uxBookList.SelectedItem != null)
+            {
+                uxSelectBook.Enabled = true;
+            }
+        }
+
+        private void uxBookList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (uxBookList.SelectedItem != null) uxSelectBook.Enabled = true;
+            else uxSelectBook.Enabled = false;
+        }
+
+        private void uxSelectBook_Click(object sender, EventArgs e)
+        {
+            Book b = books[uxBookList.SelectedIndex];
         }
     }
 }
